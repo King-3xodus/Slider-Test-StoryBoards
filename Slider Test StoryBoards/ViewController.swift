bb6d905bac54342fae803fa0bb2a086f1ff0e5a2
David Berryhill <david.berryhil@smail.astate.edu>
2020-02-25 11:17:17 -0600

    adding this because I'm behind

---
+++ Slider Test StoryBoards/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Slider Test StoryBoards
+//
+//  Created by David Berryhill on 2/12/20.
+//  Copyright © 2020 David Berryhill. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var mySlider: UISlider!
+    
+    @IBOutlet var sliderMoved: [UIView]!
+    
+    
+    
+}
+
